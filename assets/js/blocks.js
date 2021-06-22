1965ecfb38a3162f6536c14351ef35086671bdae
Gwgjnr <geoffwgg@gmail.com>
2021-06-22 11:47:18 +0000

    added consts for block colors on separate file for readability

---
+++ assets/js/blocks.js
@@ -0,0 +1,12 @@
+const blue = [1];
+
+const yellow = [1];
+
+const green = [1];
+
+const orange = [1];
+
+const red = [1];
+
+const purple = [1];
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
-const cvs = document.getElementById('game');
-const ctx = cvs.getContext("2d");
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext("2d");
 
 const row = 10;
 const col = 50;
 const sq = 50;
 const vacant = 'white';
 
-function drawsquare(x,y,_color){
+function drawsquare(x,y,color){
     ctx.fillStyle = 'white';
     ctx.fillRect(x*sq,y*sq,sq,sq);
 
@@ -33,10 +33,20 @@ function drawBoard(){
 drawBoard();
 
 let blocks = [
-    ['blue'],
-    ['yellow'],
-    ['green'],
-    ['orange'],
-    ['red'],
-    ['purple']    
-]
+    [blue, 'blue'],
+    [yellow, 'yellow'],
+    [green, 'green'],
+    [orange, 'orange'],
+    [red, 'red'],
+    [purple, 'purple']    
+];
+
+function randomPiece(){
+    let r = randomN = Math.floor(Math.random() * blocks.length)
+    return new Piece( blocks[r][0],blocks[r][1]);
+}
+
+Function block(piece, color){
+
+}
+
